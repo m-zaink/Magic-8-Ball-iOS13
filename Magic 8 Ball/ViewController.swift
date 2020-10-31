878685f63aebf9aec339e8a61ea06d123fb5f66b
m-zaink <sidsmith9@gmail.com>
2020-10-31 17:24:16 +0530

    Add motion sensing to change the ball image
    When the user shakes his/her phone, a random ball image gets rendered on the screen.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,11 +11,18 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
+    
     @IBOutlet weak var ballImageView: UIImageView!
     
-    @IBAction func askButtonPressed(_ sender: UIButton) {
-        ballImageView.image = ballArray.randomElement()
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            ballImageView.image = ballArray.randomElement()
+        }
+    }
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            ballImageView.image = ballArray.randomElement()
+        }
     }
 }
-
